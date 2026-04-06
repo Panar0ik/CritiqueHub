3bad042372dfa5d725ffc300eefb20409954d638
panar0ik <panar0ik@github.com>
2026-04-06 12:40:35 +0300

    fix tag

--- src/main/java/com/critiquehub/model/Tag.java
+++ src/main/java/com/critiquehub/model/Tag.java
@@ -7,19 +7,43 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString(exclude = "spaces")
 @Table(name = "tags")
 public class Tag {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, nullable = false)
     private String name;
 
     @ManyToMany(mappedBy = "tags")
-    private java.util.Set<Space> spaces = new java.util.HashSet<>();
+    private Set<Space> spaces = new HashSet<>();
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Tag tag = (Tag) o;
+        return id != null && id.equals(tag.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
