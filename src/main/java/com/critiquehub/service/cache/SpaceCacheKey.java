67167b551f6e2df3e08d0478ea60f88301c57cf1
panar0ik <panar0ik@github.com>
2026-04-22 22:09:04 +0300

    added cache

---
+++ src/main/java/com/critiquehub/service/cache/SpaceCacheKey.java
@@ -0,0 +1,4 @@
+package com.critiquehub.util;
+
+public record SpaceCacheKey(String tagName, int pageNumber, int pageSize) {
+}
---
+++ src/main/java/com/critiquehub/service/cache/SpaceCacheService.java
@@ -0,0 +1,37 @@
+package com.critiquehub.service;
+
+import com.critiquehub.model.Space;
+import com.critiquehub.repository.SpaceRepository;
+import com.critiquehub.util.SpaceCacheKey;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SpaceCacheService {
+
+    private final SpaceRepository spaceRepository;
+
+    private final Map<SpaceCacheKey, Page<Space>> cache = new ConcurrentHashMap<>();
+
+    public Page<Space> getSpacesByTag(final String tagName, final Pageable pageable) {
+        SpaceCacheKey key = new SpaceCacheKey(tagName, pageable.getPageNumber(), pageable.getPageSize());
+
+        return cache.computeIfAbsent(key, k -> {
+            log.info("Cache miss. Fetching from database for tag: {}", tagName);
+            return spaceRepository.findByTagNameJPQL(tagName, pageable);
+        });
+    }
+
+    public void evictCache() {
+        log.info("Cache eviction: Data has been modified.");
+        cache.clear();
+    }
+}
